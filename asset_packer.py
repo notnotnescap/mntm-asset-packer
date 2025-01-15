a5a52aa85b46295ff652c0a6892e053e15e5fb6c
nescapp <97590612+nescapp@users.noreply.github.com>
2025-01-15 21:08:14 +0100

    simpler commands

--- asset_packer.py
+++ asset_packer.py
@@ -268,10 +268,10 @@ if __name__ == "__main__":
             case "help" | "-h" | "--help":
                 print(HELP_MESSAGE)
 
-            case "create" | "setup":
+            case "create":
                 print("Not implemented yet...")
 
-            case "pack" | None:
+            case "pack":
                 if len(sys.argv) > 2:
                     if sys.argv[2] == "all":
 
